5377d67a13008f38a292dedab5ee8c288c89ceb1
vinvladi <vladislavsupra@yandex.ru>
2023-02-03 12:31:19 +0300

    ver 1.5a main file and README.md upgrade

--- ver 1.2/ver 1.3.py
+++ ver 1.2/ver 1.3.py
@@ -1,7 +1,7 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw  # Включаем в программу 2 модуля Image и ImageDraw
 
-im1 = Image.open('007.png').convert("RGBA")
-width = im1.size[0]  # считываем ее ширину(длину)
+im1 = Image.open('007.png').convert("RGBA") # Работаем с базовым изображением, которое мы будем менять
+width = im1.size[0]  # считываем ее ширину(длину) (забабахать в def данный модуль)
 height = im1.size[1]  # считываем ее высоту
 if width > height:  # длина выше высоты
     image001_crop = im1.crop((0, 0, height, height))
@@ -9,17 +9,17 @@ else:
     image001_crop = im1.crop((0, 0, width, width))
 image001_crop = image001_crop.resize((180, 180))  # по итогу у нас остается изображение с размерами 180 на 180
 
-image001_crop.save('200.png', quality=100)
-im1 = image001_crop
+image001_crop.save('200.png', quality=100) # (конец модуля) можно выход из функции def
+im1 = image001_crop # обратно перебрасываю в переменную
 
 im2 = Image.open('layer3.png').convert("RGBA")
 
-mask_im = Image.new("L", im1.size, 255)
+mask_im = Image.new("L", im1.size, 255) # модуль создания овала (причем использую 8-ми битную цветовую гамму), с 32 вроде нормально не робит, так как прозрачность не учитывается
 draw = ImageDraw.Draw(mask_im)
 draw.ellipse((7, 7, 169, 169), fill=0)
 mask_im.save('mask_circle.jpg', quality=100)  # в режиме 95 много белых кругов, тогда как в режиме 100 нет белых кругов
 
-im0 = Image.open('mask_circle.jpg').convert("L")
+im0 = Image.open('mask_circle.jpg').convert("L")  # замена переменной, скорей всего можно не конвертировать лишний раз
 
 
 im1.paste(im0, (0,0), im0)
