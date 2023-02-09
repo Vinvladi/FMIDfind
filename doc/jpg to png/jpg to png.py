baaf4c8db0985f69953e24a3c4d2bacf39d27722
vinvladi <vladislavsupra@yandex.ru>
2023-02-09 08:49:46 +0300

    Update

--- doc/jpg to png/jpg to png.py
+++ doc/jpg to png/jpg to png.py
@@ -1,4 +1,4 @@
 from PIL import Image, ImageDraw
 
-input_image = Image.open('000.jpg').convert("RGBA")
+input_image = Image.open('001.jpg').convert("RGBA")
 input_image.save('000.png', quality=100)
